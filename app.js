37ddc8a2722a0bc396b89567c501a1570f033a3f
Jessica Cardeño <cardenojessica07@gmail.com>
2022-02-12 17:42:39 -0500

    Creando estructura de archivos Node.js + express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+//app.use('/static', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.listen(3030, () => console.log("Servidor Corriendo"));
