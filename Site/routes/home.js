51b72b25d4dc69db5fa8bb938eccb43d5e1fef64
JhonDor <90356685+JhonDor@users.noreply.github.com>
2022-03-07 18:47:53 -0500

    Update home.js

--- Site/routes/home.js
+++ Site/routes/home.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-// Aća nos falta traer el controller
-const controller= require('../controllers/mainController.js');
-// Acá definimos las rutas
-router.get('/', controller.main);
 
-// Acá exportamos el resultado
-module.exports = router;
