d83ca32bc0a74351f84691e3c9b4d19ee39ca8e6
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-03-27 21:31:56 -0500

    Agregando estructura para detalle de un producto particular

--- Site/controllers/productsController.js
+++ Site/controllers/productsController.js
@@ -1,15 +1,24 @@
-const productsController ={
-    addEditProduct: (req,res) =>{
+const path = require('path');
+const fs = require('fs');
+
+const productsJSONPath = path.join(__dirname, '../data/products.JSON');
+let products = JSON.parse(fs.readFileSync(productsJSONPath, 'utf-8'));
+
+const productsController = {
+    addEditProduct: (req, res) => {
         res.render('products/add_editProduct');
     },
-    listaProductos: (req,res) =>{
+    listaProductos: (req, res) => {
         res.render('products/listaProductos');
     },
-    productCart: (req,res) => {
+    productCart: (req, res) => {
         res.render("products/productCart")
     },
-    productDetail: (req,res) =>{
-        res.render('products/productDetail');
+    productDetail: (req, res) => {
+        let id = req.params.id;
+        let product = products.find(product => product.id == id);
+
+        res.render('products/productDetail', { product, index: 0 });
     },
 }
 
--- Site/routes/products.js
+++ Site/routes/products.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const productsController = require('../controllers/productsController.js');
 
 router.get("/add_editProduct", productsController.addEditProduct);
-router.get("/listaProductos", productsController.listaProductos);
+router.get("/listaProductos", productsController.listaProductos); // -> Corregir nombre
 router.get("/productCart", productsController.productCart);
-router.get("/productDetail", productsController.productDetail);
+router.get("/productDetail/:id", productsController.productDetail);
 
 module.exports = router;
