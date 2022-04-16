d3b8abb75d8deef45fc2e879e99ac24949e3e25b
Jessica Cardeño <cardenojessica07@gmail.com>
2022-04-16 14:57:49 -0500

    Implementando middleware en el post del login y configurando ruta logout

--- Site/routes/users.js
+++ Site/routes/users.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const usersController = require('../controllers/usersController.js');
 
+const loginValidations = require("../middlewares/loginValidator");
+
 router.get("/login", usersController.login);
-router.post("/login", usersController.check)
+router.post("/login", loginValidations, usersController.check);
 
 router.get("/register", usersController.registro);
+router.get("/logout", usersController.logout);
 //crear usuario
 router.post("/create", usersController.createUser);
 module.exports = router;
