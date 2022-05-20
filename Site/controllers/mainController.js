3b5cd96cca45549f5d07b55c24a6b0559d00c200
Jessica Cardeño <cardenojessica07@gmail.com>
2022-05-20 17:28:42 -0500

    Actualizando vistas con base de datos, actualizando estilos

--- Site/controllers/mainController.js
+++ Site/controllers/mainController.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+// Importando DB
+const db = require("../database/models");
 
 //Cargar desde el archivo JSON
 let JSONPath = (name) => path.join(__dirname, '../data/' + name);
@@ -9,7 +11,14 @@ let resultReadJSON = (JSONPath) => JSON.parse(fs.readFileSync(JSONPath, 'utf-8')
 let products = resultReadJSON(JSONPath('products.json'));
 
 const mainController = {
-    home: (req, res) => {
+    home: async (req, res) => {
+        let products = await db.Producto.findAll({
+            include: [
+                {association: "images"},
+            ]
+        })
+
+        console.log(JSON.stringify(products, null, 2));
 
         let productsWithDiscount = products.filter(product => product.discount != 0);
         let productsWithoutDiscount = products.filter(product => product.discount == 0);
