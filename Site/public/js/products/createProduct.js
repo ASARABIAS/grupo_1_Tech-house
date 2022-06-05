30b96205d882f54537f92d8020c36501ab300ffd
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-06-05 17:16:15 -0500

    Agregando las validaciones de producto

--- Site/public/js/products/createProduct.js
+++ Site/public/js/products/createProduct.js
@@ -2,11 +2,11 @@ window.onload = () => {
 
     const productImage = document.getElementById("productImage");
     const imagePreview = document.querySelectorAll(".imagePreview");
-    productImage.addEventListener("change", function(e){
-        if(e.target.value){
+    productImage.addEventListener("change", function(e) {
+        if (e.target.value) {
             // this ---> Input file (Elemento el cual presenta el evento)
             readURL(this);
-        }else{
+        } else {
             imagePreview.forEach(image => {
                 image.setAttribute("src", "/images/logo.png");
             });
@@ -17,7 +17,7 @@ window.onload = () => {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
-            reader.onload = function (e) {
+            reader.onload = function(e) {
                 //alert(e.target.result);
                 imagePreview.forEach(image => {
                     image.setAttribute("src", e.target.result);
--- Site/public/js/validationProduct.js
+++ Site/public/js/validationProduct.js
@@ -1,5 +1,28 @@
 //Validations for create and edit product
-addEventListener('load', () => {
-    const inputName = document.querySelector('#name');
-    const inputSpecification = document.querySelector('#specifications');
+const form = document.querySelector('form');
+const inputImage = document.querySelector('#productImage');
+const inputName = document.querySelector('#name');
+const inputSpecification = document.querySelector('#specifications');
+const smallImage = document.querySelector('#errorProductImage');
+const smallName = document.querySelector('#errorName');
+const smallSpecification = document.querySelector('#errorSpecifications');
+
+console.log('form: ', form);
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    alert("entro");
+
+    const ext = (inputImage.value.length > 0) ? inputImage.value.split(".").pop() : "none";
+
+    if (ext != 'jpg' || ext != 'jpeg' || ext != 'png' || ext != 'gif') aux(event, smallImage, `La estencion ${ext} no es valida; Extenciones valodas jpg, jpeg, png, gif `);
+    if (inputName.value.length < 5) aux(event, smallName, 'El nombre es muy corto');
+    if (inputSpecification.value.length < 20) aux(event, smallSpecification, 'Las Especificaciones es muy corta');
+
 });
+
+function aux(event, small, messenger) {
+    small.innerText = messenger;
+    return;
+}
