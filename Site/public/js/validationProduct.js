8e641c4b3fd1fa7178515202df3eb39ad7194072
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-06-05 15:18:30 -0500

    ralizar merge con main

---
+++ Site/public/js/validationProduct.js
@@ -0,0 +1,5 @@
+//Validations for create and edit product
+addEventListener('load', () => {
+    const inputName = document.querySelector('#name');
+    const inputSpecification = document.querySelector('#specifications');
+});
