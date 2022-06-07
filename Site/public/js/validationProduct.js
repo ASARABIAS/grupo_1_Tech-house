5ca72c5fb1138e7ed3519d31e41b65e1c74f03d6
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-06-06 20:33:20 -0500

    mejorando al metodo de jhon

--- Site/public/js/validationProduct.js
+++ Site/public/js/validationProduct.js
@@ -3,24 +3,26 @@ const form = document.querySelector('form.infor-product');
 const inputImage = document.querySelector('#productImage');
 const inputName = document.querySelector('#name');
 const inputSpecification = document.querySelector('#specifications');
-const smallImage = document.querySelector('#errorProductImage');
-const smallName = document.querySelector('#errorName');
-const smallSpecification = document.querySelector('#errorSpecifications');
+const spanImage = document.querySelector('#errorProductImage');
+const spanName = document.querySelector('#errorName');
+const spanSpecification = document.querySelector('#errorSpecifications');
 
 form.addEventListener('submit', (event) => {
+    
     const ext = (inputImage.value.length > 0) ? inputImage.value.split(".").pop() : "none";
-    const exts = []
+
     clear();
+    
 
-    if (!(ext == 'jpg' || ext == 'jpeg' || ext == 'png' || ext == 'gif')) smallImage.innerText = `La estencion ${ext} no es valida; Las extenciones validas son: jpg, jpeg, png, gif `;
-    if (inputName.value.length < 5) smallName.innerText = 'El nombre es muy corto';
-    if (inputSpecification.value.length < 20) smallSpecification.innerText = 'Las Especificaciones es muy corta';
+    if (!(ext == 'jpg' || ext == 'jpeg' || ext == 'png' || ext == 'gif')) spanImage.innerText = `La estencion ${ext} no es valida; Las extenciones validas son: jpg, jpeg, png, gif `;
+    if (inputName.value.length < 5) spanName.innerText = 'El nombre es muy corto';
+    if (inputSpecification.value.length < 20) spanSpecification.innerText = 'Las Especificaciones es muy corta';
 
-    if (smallName.innerText.length > 0 || smallImage.innerText.length > 0 || smallSpecification.innerText.length > 0) event.preventDefault();
+    if (spanName.innerText.length > 0 || spanImage.innerText.length > 0 || spanSpecification.innerText.length > 0) event.preventDefault();
 });
 
 function clear() {
-    smallName.innerText = "";
-    smallImage.innerText = "";
-    smallSpecification.innerText = "";
+    spanName.innerText = "";
+    spanImage.innerText = "";
+    spanSpecification.innerText = "";
 }
