6c4a7dc95c8d7364b9ba9bc1a482d45e09ae7b14
JhonDor <90356685+JhonDor@users.noreply.github.com>
2022-06-17 18:38:29 -0500

    se arrela la validación del back que no tenia texto, se arreglan los mensajes, aun no se limpian los errores del front

--- Site/public/js/users/registerValidations.js
+++ Site/public/js/users/registerValidations.js
@@ -4,123 +4,90 @@ const userName = document.querySelector("#name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const country = document.querySelector("#country");
-
+const nameErrors = document.querySelector("#nameError");
+const emailErrors = document.querySelector("#emailErrors");
+const passwordErrors = document.querySelector("#passwordError");
+const countryErrors = document.querySelector("#countryError");
+const avatarErrors = document.querySelector("#avatarError");
 
 registerForm.addEventListener("submit", (e) => {
+  
   const userNameValue = userName.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const countryValue = country.value.trim();
   const avatarValue = avatar.value.trim();
-  let nameErrors = [];
-  let emailErrors = [];
-  let passwordErrors = [];
-  let countryErrors =[];
-  let avatarErrors = [];
+  clear();
+ 
 
-  if (userNameValue == ''|| userNameValue == null) {
-    
-    nameErrors.push("El nombre no puede estar vacio");
-  }
-  else if(userNameValue.length <3){
-    nameErrors.push("El nombre debe tener mínimo 2 caracteres");
+  if (userNameValue == "") {
+    nameErrors.innerText = "El campo de nombre no puede estar vacío"}
+  
+  else if (userNameValue.length < 3) {
+    nameErrors.innerText = "El nombre debe tener mínimo 2 caracteres";
   };
-  if(nameErrors.length > 0) {
-    e.preventDefault();
-    let ulErrors= document.querySelector(".registerName div.error ul");
-    ulErrors.innerHTML = "";
-    for( let i =0; i< nameErrors.length;i++){
-      ulErrors.innerHTML += "<li>" +nameErrors[i]+ "</li>"
-    }
 
-}
-
-if (emailValue == ''|| emailValue == null) {
-    
-  emailErrors.push("El campo de correo electrónico no puede estar vacio");
-}
-
-else if (!isEmail(emailValue)) {
-  emailErrors.push("Debe escribir un formato de correo electrónico valido");
-}
-
-if(emailErrors.length > 0) {
-  e.preventDefault();
-  let ulErrors= document.querySelector(".registerEmail div.error ul");
-  ulErrors.innerHTML = "";
-  for( let i =0; i< emailErrors.length;i++){
-    ulErrors.innerHTML += "<li>" +emailErrors[i]+ "</li>"
+  if (emailValue == "") {
+    emailErrors.innerText =
+      "El campo de correo electrónico no puede estar vacío";
   }
-  
-}
-if (passwordValue == ''|| passwordValue == null) {
-    
-  passwordErrors.push("El campo de contraseña no puede estar vacio");
-}
+  else if (!isEmail(emailValue)) {
+    emailErrors.innerText =
+      "Debe escribir un formato de correo electrónico validoo";
+  };
 
-else if(passwordValue.length <8){
-  passwordErrors.push("La contraseña debe tener mínimo 8 caracteres");
-}
-
-else if (!validPassword(passwordValue)) {
-  passwordErrors.push("La contraseña deberá tener letras mayúsculas, minúsculas, un número y un carácter especial");
-}
-
-if(passwordErrors.length > 0) {
-  e.preventDefault();
-  let ulErrors= document.querySelector(".registerPassword div.error ul");
-  ulErrors.innerHTML = "";
-  for( let i =0; i< passwordErrors.length;i++){
-    ulErrors.innerHTML += "<li>" +passwordErrors[i]+ "</li>"
+  if (passwordValue == "") {
+    passwordErrors.innerText = "El campo de contraseña no puede estar vacío";
   }
-  
-}
-
-if (countryValue == ''|| countryValue == null) {
-    
-  countryErrors.push("El campo de país no puede estar vacio");
-}
-
-if(countryErrors.length > 0) {
-  e.preventDefault();
-  let ulErrors= document.querySelector(".registerCountry div.error ul");
-  ulErrors.innerHTML = "";
-  for( let i =0; i< countryErrors.length;i++){
-    ulErrors.innerHTML += "<li>" +countryErrors[i]+ "</li>"
+  else if (passwordValue.length < 8) {
+    passwordErrors.innerText = "La contraseña debe tener mínimo 8 caracteres";
   }
+  else if (!validPassword(passwordValue)) {
+    passwordErrors.innerText =
+      "La contraseña debe tener mínimo una letra mayúscula, una letra minúscula, un número y un carácter especial";
+  };
+  if (countryValue == "") {
+    countryErrors.innerText = "El campo de país no puede estar vacío";
+  };
+  if (avatarValue !== "") {
+    if (!validImage(avatarValue)) {
+      avatarErrors.innerText =
+        "Solo puede usar imágenes con los formatos: JPG, JPEG, PNG o GIF como su imagen de perfil";
+    }};
+if (
+      nameErrors.innerText.length > 0 ||
+      emailErrors.innerText.length > 0 ||
+      passwordErrors.innerText.length > 0 ||
+      countryErrors.innerText.length > 0 ||
+      avatarErrors.innerText.length > 0
+    )e.preventDefault();
   
-}
-if(avatarValue !== ""){
-if(!validImage(avatarValue) ){
-  avatarErrors.push("Solo puede usar imagenes con los formatos: JPG, JPEG, PNG o GIF como su imagen de perfil")
-
-}
-}
-
-if(avatarErrors.length > 0) {
-  e.preventDefault();
-  let ulErrors= document.querySelector(".registerAvatar div.error ul");
-  ulErrors.innerHTML = "";
-  for( let i =0; i< avatarErrors.length;i++){
-    ulErrors.innerHTML += "<li>" +avatarErrors[i]+ "</li>"
-  }
-  
-}
-
 });
 
 function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
 
-function validPassword(password){
-  return  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(password);
+function validPassword(password) {
+  return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(
+    password
+  );
 }
 
-function validImage(image){
+function validImage(image) {
   return /.*\.(gif|jpe?g|bmp|png)*$/.test(image);
 }
-    
+
+function clear() {
+  nameErrors.innerText = "";
+  emailErrors.innerText = "";
+  passwordErrors.innerText = "";
+  countryErrors.innerText = "";
+  avatarErrors.innerText = "";
+}
+
   
 
 
