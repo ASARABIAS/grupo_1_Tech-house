ad70d3a75991306e1829e6e4f101707d7f688afa
Adan2Net <adaneduardo3015228321@gmail.com>
2022-06-21 17:36:38 -0500

    local

---
+++ Dashboard/src/components/mains/product/Categories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import ContentRowCards from '../../ContentRowCards';
+import Footer from '../../partials/Footer';
+import TopBar from '../../partials/TopBar';
+import Image from '../../Image';
+
